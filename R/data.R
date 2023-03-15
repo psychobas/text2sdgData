06797d8910d6e519475009e6d97e497a08ead4e1
Dominik Meier <dominikmeier@outlook.com>
2023-03-15 13:06:07 +0100

    ..

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
-#' Ensemble models based on a random forest architecture that pools the predictions of six labeling systems generated using `detect_sdg_systems()` and also considers text length.
-#'
 #' A list of trained `ranger::ranger()` random forest models that are used by the text2sdg `detect_sdg()` function.
 #'
-#' @source Wulff, Meier, & Mata (2023)
+#' Ensemble models based on a random forest architecture that pools the predictions of six labeling systems generated using the `detect_sdg_systems()` function from the text2sdg package and also considers text length.
+#'
+#' @source Wulff, D. U., Meier, D., & Mata, R. (2023). Using novel data and ensemble models to improve automated SDG-labeling. arXiv
 "ensembles"
